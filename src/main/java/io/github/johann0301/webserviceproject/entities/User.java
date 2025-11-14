218108f79b616ae1047dce4b0da35d5b886e66d8
johann0301 <johann.bezerra27@gmail.com>
2025-11-14 18:07:35 -0300

    Class Order - Instant, Part 1

--- src/main/java/io/github/johann0301/webserviceproject/entities/User.java
+++ src/main/java/io/github/johann0301/webserviceproject/entities/User.java
@@ -3,6 +3,8 @@ package io.github.johann0301.webserviceproject.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -78,5 +83,9 @@ public class User implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
